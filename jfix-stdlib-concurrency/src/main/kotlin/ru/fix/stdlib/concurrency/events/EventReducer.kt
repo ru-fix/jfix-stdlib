6bc1a34aa04e673866b0cf39e27f6bc2d250b6ca
akazankov <akazankov@fix.ru>
2020-05-15 09:56:52 +0300

    EventReducer.kt created with tests

---
+++ jfix-stdlib-concurrency/src/main/kotlin/ru/fix/stdlib/concurrency/events/EventReducer.kt
@@ -0,0 +1,77 @@
+package ru.fix.stdlib.concurrency.events
+
+import mu.KotlinLogging
+import java.util.concurrent.*
+import java.util.concurrent.locks.ReentrantLock
+
+
+/**
+ * EventReducer invokes given [handler] when [handle] function was invoked.
+ * If [handle] function was invoked (one or many times) during [handler] working,
+ * [handler] will be invoked after completing and so on.
+ *
+ * Therefore if [handle] function was invoked 100 times with breaks in 1 millisecond,
+ * and [handler]'s work takes 70 milliseconds,
+ * then [handler] wasn't invoked 100 times, but 3 times, consistently.
+ * */
+class EventReducer(
+        private val handler: () -> Unit,
+        private val shutdownCheckPeriodMs: Long = 1_000,
+        private val awaitTerminationPeriodMs: Long = 60_000
+) : AutoCloseable {
+    private val eventReceivingExecutor = Executors.newSingleThreadExecutor()
+
+    private val awaitingEventQueue = ArrayBlockingQueue<Any>(1)
+
+    fun handle() = synchronized(awaitingEventQueue) {
+        if (awaitingEventQueue.isEmpty()) {
+            awaitingEventQueue.put(Any())
+        }
+    }
+
+    fun start() {
+        CompletableFuture.runAsync(Runnable {
+            while (true) {
+                when (awaitEventOrShutdown()) {
+                    AwaitingResult.EVENT -> handler.invoke()
+                    AwaitingResult.SHUTDOWN -> return@Runnable
+                    AwaitingResult.ERROR -> {
+                    }
+                }
+            }
+        }, eventReceivingExecutor)
+    }
+
+    private enum class AwaitingResult {
+        EVENT, SHUTDOWN, ERROR
+    }
+
+    private fun awaitEventOrShutdown(): AwaitingResult {
+        try {
+            while (awaitingEventQueue.poll(shutdownCheckPeriodMs, TimeUnit.MILLISECONDS) == null) {
+                if (eventReceivingExecutor.isShutdown) {
+                    return AwaitingResult.SHUTDOWN
+                }
+            }
+        } catch (e: Exception) {
+            log.error("waiting event was interrupted", e)
+            return AwaitingResult.ERROR
+        }
+        synchronized(awaitingEventQueue) {
+            awaitingEventQueue.clear()
+        }
+        return AwaitingResult.EVENT
+    }
+
+    override fun close() {
+        eventReceivingExecutor.shutdown()
+        if (!eventReceivingExecutor.awaitTermination(awaitTerminationPeriodMs, TimeUnit.MILLISECONDS)) {
+            log.warn("Failed to wait eventReceivingExecutor termination")
+            eventReceivingExecutor.shutdownNow()
+        }
+    }
+
+    companion object {
+        private val log = KotlinLogging.logger {}
+    }
+}
---
+++ jfix-stdlib-concurrency/src/test/kotlin/ru/fix/stdlib/concurrency/events/EventReducerTest.kt
@@ -0,0 +1,102 @@
+package ru.fix.stdlib.concurrency.events
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.ReentrantLock
+
+internal class EventReducerTest {
+
+    @Test
+    fun `WHEN handle single time THEN handler invoked one time`() {
+        val invokes = ArrayBlockingQueue<Any>(1)
+        EventReducer(handler = {
+            invokes.put(Any())
+        }).use {
+            it.start()
+            it.handle()
+            assertNotNull(invokes.poll(1, TimeUnit.SECONDS))
+            assertNull(invokes.poll(1, TimeUnit.SECONDS))
+        }
+    }
+
+    @Test
+    fun `WHEN reducer triggered several times at once AND handler is slow THEN handler invoked one or two times`() {
+        val eventsQuantity = 10
+        val invokes = ArrayBlockingQueue<Any>(2)
+        val awaitingEventsFromAllThreadsLatch = CountDownLatch(eventsQuantity)
+        EventReducer(handler = {
+            invokes.put(Any())
+            awaitingEventsFromAllThreadsLatch.await() //simulate slow handler
+        }).use { reducer ->
+            reducer.start()
+
+            val executor = Executors.newFixedThreadPool(eventsQuantity)
+            repeat(eventsQuantity) {
+                executor.execute {
+                    reducer.handle()
+                    awaitingEventsFromAllThreadsLatch.countDown()
+                }
+            }
+
+            assertNotNull(invokes.poll(1, TimeUnit.SECONDS))
+            invokes.poll(1, TimeUnit.SECONDS)
+            assertNull(invokes.poll(1, TimeUnit.SECONDS))
+
+            executor.shutdown()
+        }
+    }
+
+    @Test
+    fun `WHEN handle event after invoking handler THEN handler will be invoked after completing`() {
+        val invokes = ArrayBlockingQueue<Any>(1)
+        val holdHandlerInvocationLock = ReentrantLock().apply { lock() }
+        EventReducer(handler = {
+            invokes.put(Any())
+            holdHandlerInvocationLock.lock()
+        }).use { reducer ->
+            reducer.start()
+            reducer.handle()
+            assertNotNull(invokes.poll(1, TimeUnit.SECONDS))
+            reducer.handle()
+            holdHandlerInvocationLock.unlock()
+            assertNotNull(invokes.poll(1, TimeUnit.SECONDS))
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("awaitTerminationPeriodMs And ShutdownCheckPeriodMs")
+    fun `WHEN reducer was shutdown THEN handler not invoked`(
+            awaitTerminationPeriodMs: Long, shutdownCheckPeriodMs: Long
+    ) {
+        val invokes = ArrayBlockingQueue<Any>(1)
+        val eventReducer = EventReducer(
+                awaitTerminationPeriodMs = awaitTerminationPeriodMs,
+                shutdownCheckPeriodMs = shutdownCheckPeriodMs,
+                handler = {
+                    invokes.put(Any())
+                }
+        )
+        eventReducer.start()
+        eventReducer.handle()
+        assertNotNull(invokes.poll(1, TimeUnit.SECONDS))
+        eventReducer.close()
+        assertNull(invokes.poll(1, TimeUnit.SECONDS))
+    }
+
+    @Suppress("unused")
+    companion object {
+        @JvmStatic
+        fun `awaitTerminationPeriodMs And ShutdownCheckPeriodMs`() = listOf(
+                Arguments.of(60_000L, 1_000L),
+                Arguments.of(1L, 1_000L)
+        )
+    }
+}
