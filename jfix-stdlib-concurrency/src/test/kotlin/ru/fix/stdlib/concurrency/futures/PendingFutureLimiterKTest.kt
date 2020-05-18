d2b25c47f29e7906770f501a2859aaf668be3d66
akazankov <akazankov@fix.ru>
2020-05-18 15:02:26 +0300

    issue-51 improve PendingFutureLimiterKTest.kt

--- jfix-stdlib-concurrency/src/test/kotlin/ru/fix/stdlib/concurrency/futures/PendingFutureLimiterKTest.kt
+++ jfix-stdlib-concurrency/src/test/kotlin/ru/fix/stdlib/concurrency/futures/PendingFutureLimiterKTest.kt
@@ -1,6 +1,5 @@
 package ru.fix.stdlib.concurrency.futures
 
-import mu.KotlinLogging
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertTimeoutPreemptively
 import ru.fix.dynamic.property.api.DynamicProperty
@@ -10,27 +9,27 @@ import java.util.concurrent.TimeUnit
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
 
-private val internalTimeoutMs = TimeUnit.MINUTES.toMillis(1)
+private val assertTimeoutDuration = Duration.ofSeconds(10)
+private val internalLargeTimeoutMs: Long = TimeUnit.MINUTES.toMillis(1)
 private const val CHECKS_QUANTITY = 100
 
 internal class PendingFutureLimiterKTest {
 
-
     private val limiter = PendingFutureLimiter(
             DynamicProperty.of(2),
-            DynamicProperty.of(internalTimeoutMs)
+            DynamicProperty.of(internalLargeTimeoutMs)
     )
 
 
     @Test
     fun `waitAll with timeout WHEN enqueue completed future AND breaks exist THEN limiter doesn't stuck`() {
         var checkCounter = 0
-        assertTimeoutPreemptively(Duration.ofSeconds(10), { "failed on $checkCounter check" }) {
+        assertTimeoutPreemptively(assertTimeoutDuration, { "failed on $checkCounter check" }) {
             while (checkCounter < CHECKS_QUANTITY) {
                 checkCounter++
                 limiter.enqueueBlocking(CompletableFuture.completedFuture(null))
                 Thread.sleep(10)
-                limiter.waitAll(internalTimeoutMs)
+                limiter.waitAll(internalLargeTimeoutMs)
             }
         }
     }
@@ -38,12 +37,12 @@ internal class PendingFutureLimiterKTest {
     @Test
     fun `waitAll with timeout WHEN enqueue 2 completed future THEN limiter doesn't stuck`() {
         var checkCounter = 0
-        assertTimeoutPreemptively(Duration.ofSeconds(10), { "failed on $checkCounter check" }) {
+        assertTimeoutPreemptively(assertTimeoutDuration, { "failed on $checkCounter check" }) {
             while (checkCounter < CHECKS_QUANTITY) {
                 checkCounter++
                 limiter.enqueueBlocking(CompletableFuture.completedFuture(null))
                 limiter.enqueueBlocking(CompletableFuture.completedFuture(null))
-                limiter.waitAll(internalTimeoutMs)
+                limiter.waitAll(internalLargeTimeoutMs)
             }
         }
     }
@@ -51,11 +50,11 @@ internal class PendingFutureLimiterKTest {
     @Test
     fun `waitAll with timeout WHEN enqueue completed future THEN limiter doesn't stuck`() {
         var checkCounter = 0
-        assertTimeoutPreemptively(Duration.ofSeconds(10), { "failed on $checkCounter check" }) {
+        assertTimeoutPreemptively(assertTimeoutDuration, { "failed on $checkCounter check" }) {
             while (checkCounter < CHECKS_QUANTITY) {
                 checkCounter++
                 limiter.enqueueBlocking(CompletableFuture.completedFuture(null))
-                limiter.waitAll(internalTimeoutMs)
+                limiter.waitAll(internalLargeTimeoutMs)
             }
         }
     }
@@ -63,7 +62,7 @@ internal class PendingFutureLimiterKTest {
     @Test
     fun `waitAll without timeout WHEN enqueue completed future THEN limiter doesn't stuck`() {
         var checkCounter = 0
-        assertTimeoutPreemptively(Duration.ofSeconds(10), { "failed on $checkCounter check" }) {
+        assertTimeoutPreemptively(assertTimeoutDuration, { "failed on $checkCounter check" }) {
             while (checkCounter < CHECKS_QUANTITY) {
                 checkCounter++
                 limiter.enqueueBlocking(CompletableFuture.completedFuture(null))
@@ -76,13 +75,13 @@ internal class PendingFutureLimiterKTest {
     fun `waitAll with timeout WHEN enqueue not completed future THEN limiter doesn't stuck`() {
         val lock = ReentrantLock()
         var checkCounter = 0
-        assertTimeoutPreemptively(Duration.ofSeconds(10), { "failed on $checkCounter check" }) {
+        assertTimeoutPreemptively(assertTimeoutDuration, { "failed on $checkCounter check" }) {
             while (checkCounter < CHECKS_QUANTITY) {
                 checkCounter++
                 lock.lock()
                 limiter.enqueueBlocking(CompletableFuture.runAsync { lock.withLock {} })
                 lock.unlock() //ensure future wasn't completed before being queued
-                limiter.waitAll(internalTimeoutMs)
+                limiter.waitAll(internalLargeTimeoutMs)
             }
         }
     }
@@ -91,7 +90,7 @@ internal class PendingFutureLimiterKTest {
     fun `waitAll without timeout WHEN enqueue not completed future THEN limiter doesn't stuck`() {
         val lock = ReentrantLock()
         var checkCounter = 0
-        assertTimeoutPreemptively(Duration.ofSeconds(10), { "failed on $checkCounter check" }) {
+        assertTimeoutPreemptively(assertTimeoutDuration, { "failed on $checkCounter check" }) {
             while (checkCounter < CHECKS_QUANTITY) {
                 checkCounter++
                 lock.lock()
@@ -105,7 +104,7 @@ internal class PendingFutureLimiterKTest {
     @Test
     fun `waitAll with timeout WHEN enqueue unlimited completed future THEN limiter doesn't stuck`() {
         var checkCounter = 0
-        assertTimeoutPreemptively(Duration.ofSeconds(10), { "failed on $checkCounter check" }) {
+        assertTimeoutPreemptively(assertTimeoutDuration, { "failed on $checkCounter check" }) {
             while (checkCounter < CHECKS_QUANTITY) {
                 checkCounter++
                 limiter.enqueueUnlimited(CompletableFuture.completedFuture(null))
@@ -114,7 +113,13 @@ internal class PendingFutureLimiterKTest {
         }
     }
 
-    companion object {
-        val log = KotlinLogging.logger { }
+    @Test
+    fun `waitAll with timeout WHEN enqueued slow future THEN limiter doesn't stuck`() {
+        assertTimeoutPreemptively(assertTimeoutDuration) {
+            limiter.enqueueUnlimited(CompletableFuture.runAsync {
+                Thread.sleep(assertTimeoutDuration.toMillis() / 2)
+            })
+            limiter.waitAll(internalLargeTimeoutMs)
+        }
     }
 }
